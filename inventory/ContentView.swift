d4a664112cb4b7664013cdb3205f7d0626844ade
contyadvait <advaitthepro@outlook.com>
2024-06-28 22:47:05 +0800

    Initial Commit

---
+++ inventory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  inventory
+//
+//  Created by Milind Contractor on 28/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ inventory/inventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  inventoryApp.swift
+//  inventory
+//
+//  Created by Milind Contractor on 28/6/24.
+//
+
+import SwiftUI
+
+@main
+struct inventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
